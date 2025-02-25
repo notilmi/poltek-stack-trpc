7f22562f5a2bde1d0e13d7933cf600778d38f4c9
Achmad Ilmi Al Akbar <dznilmi@gmail.com>
2025-02-25 10:29:49 +0700

    feat: authjs schema

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth";
+import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Resend({
+      apiKey: process.env.RESEND_API_KEY ?? "",
+      from: process.env.RESEND_FROM ?? "",
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID ?? "",
+      clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+    }),
+  ],
 });
