b8bf60fc5fc7caa945dba3dcce7edaa6753e0ffe
Achmad Ilmi Al Akbar <dznilmi@gmail.com>
2025-03-08 21:53:47 +0700

    feat: Agreement Page

---
+++ src/app/(agreement)/terms-of-service/page.tsx
@@ -0,0 +1,44 @@
+function TOSPage() {
+  return (
+    <div className="prose lg:prose-lg mx-auto h-screen max-w-screen-md p-4 py-12 text-justify">
+      <h1>Terms Of Service</h1>
+      <p>Last updated: March 08, 2025</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at
+        congue massa, in tincidunt odio. Nulla eget sodales nisi, nec placerat
+        turpis. Nam aliquet placerat lorem, sit amet pulvinar urna bibendum ac.
+        Pellentesque nec semper dolor. Ut tellus justo, bibendum vitae
+        scelerisque in, tristique in justo. Quisque accumsan turpis vitae dolor
+        sodales iaculis. Nam hendrerit libero vitae lacus volutpat, venenatis
+        porttitor magna ornare. Donec tempor pulvinar aliquam. Duis varius,
+        felis eget congue dapibus, nisi ipsum sagittis velit, tincidunt pretium
+        nisl metus et ipsum. Sed euismod ultrices sodales. Suspendisse at mattis
+        purus. Aenean cursus volutpat nulla, ut tempor nulla.
+      </p>
+      <p>
+        In lectus velit, pharetra nec feugiat id, faucibus sed diam. Ut cursus
+        dignissim tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Integer vestibulum magna nec enim aliquet, ac luctus justo
+        tincidunt. Proin pulvinar enim quis nunc iaculis molestie. Vivamus
+        aliquet, ex a luctus dictum, leo purus imperdiet leo, et bibendum orci
+        quam eu ante. Duis tincidunt tempor vehicula. Nulla in efficitur diam.
+        Morbi sit amet scelerisque eros.
+      </p>
+      <p>
+        Proin accumsan ante id dictum sodales. Quisque suscipit ipsum in mauris
+        dignissim rhoncus. Vestibulum ac nibh in nunc lobortis viverra non non
+        odio. Pellentesque ultricies eros a pretium euismod. Pellentesque
+        finibus eleifend consectetur. Cras viverra nec ante quis ultrices.
+        Pellentesque molestie augue ac lacinia commodo. Ut in consequat purus,
+        ut sollicitudin nibh. Phasellus porta, metus vitae convallis posuere,
+        ante lacus gravida leo, sit amet scelerisque velit ipsum a urna. Mauris
+        eu magna enim. Sed ac tellus metus. Nam faucibus nisi vel faucibus
+        rhoncus. Sed varius libero in orci tristique, vel aliquet ipsum
+        pulvinar. Sed viverra metus ut dolor ultricies, et aliquam orci
+        ullamcorper. Nam id dui commodo, venenatis tortor eu, vehicula libero.
+      </p>
+    </div>
+  );
+}
+
+export default TOSPage;
--- src/app/auth/auth-form.tsx
+++ src/app/auth/auth-form.tsx
@@ -118,8 +118,9 @@ export async function AuthForm({
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        By clicking continue, you agree to our{" "}
+        <a href="/terms-of-service">Terms of Service</a> and{" "}
+        <a href="/privacy-policy">Privacy Policy</a>.
       </div>
     </div>
   );
