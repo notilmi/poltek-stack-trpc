12554dbc15db5e0f2a95b08196c4e7c3a79708d7
Achmad Ilmi Al Akbar <dznilmi@gmail.com>
2025-03-10 19:46:32 +0700

    fix: ID in user being optional

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,12 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { authConfig } from "./auth.config";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
